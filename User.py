91a278863f4572710e9c77822d02e11ea38b3ae1
kelvin389 <kelvinl.leung@gmail.com>
2023-12-22 06:59:17 -0800

    move user class to its own file

---
+++ User.py
@@ -0,0 +1,61 @@
+import re
+
+STATUS_HOST = 0
+STATUS_UNDECIDED = 1
+STATUS_ACCEPTED = 2
+STATUS_REJECTED = 3
+STATUS_MAYBE = 4
+
+EMOJI_HOST = "👑"
+EMOJI_UNDECIDED = "❔"
+EMOJI_ACCEPTED = "✅"
+EMOJI_REJECTED = "❌"
+EMOJI_MAYBE = "🤔"
+
+class User:
+    id_str: str
+    id: int
+    _status: int
+    emoji: str
+    note: str
+
+    # id_input is an integer containing only the user id when host=True
+    # id_input is a string of format <@[id]> when host=False
+    def __init__(self, id_input, host=False):
+        self.note = ""
+
+        if host:
+            self.id = id_input
+            self.id_str = "<@" + str(id_input) + ">"
+            self.status = 0
+            self.emoji = EMOJI_HOST
+            #set_status(0)
+        else:
+            # extract id from string
+            match = re.search(r"<@(\d+)>", id_input)
+            self.id = int(match.group(1))
+            self.id_str = id_input
+            self.status = 1
+            self.emoji = EMOJI_UNDECIDED
+            #set_status(1)
+
+    def set_status(self, status):
+        self._status = status
+        
+        if status == STATUS_HOST:
+            self.emoji = EMOJI_HOST
+        elif status == STATUS_UNDECIDED:
+            self.emoji = EMOJI_UNDECIDED
+        elif status == STATUS_ACCEPTED:
+            self.emoji = EMOJI_ACCEPTED
+        elif status == STATUS_REJECTED:
+            self.emoji = EMOJI_REJECTED
+        elif status == STATUS_MAYBE:
+            self.emoji = EMOJI_MAYBE
+        else:
+            self.emoji = "how"
+
+    def get_status(self):
+        return self._status
+
+    status = property(get_status, set_status)
