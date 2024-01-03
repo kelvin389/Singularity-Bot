71e2fff0e1013632a142649239780c3a6b158684
BenAstromo <bennett.hannemann@hotmail.com>
2024-01-02 19:54:03 -0800

    Event class now also stores event (event string), datetime, and timestamp

--- Event.py
+++ Event.py
@@ -5,8 +5,11 @@ class Event:
     embed_base: str
     embed_str: str
 
-    def __init__(self, users):
-        self.users = users
+    def __init__(self, event, users, datetime, timestamp):
+        self._event = event
+        self._users = users
+        self._datetime = datetime
+        self._timestamp = timestamp
 
     def update_embed_str(self):
         new_embed_str = ''
@@ -17,6 +20,14 @@ class Event:
         # maybe have a new var for if Users is updated and automatically all this function
         # whenever embed_str is retrieved if modified
             
+    def set_event(self, event):
+        self._event = event
+
+    def get_event(self):
+        return self._event
+
+    event = property(get_event, set_event)
+            
     def set_users(self, users):
         self._users = users
 
@@ -24,3 +35,21 @@ class Event:
         return self._users
 
     users = property(get_users, set_users)
+
+
+    def set_datetime(self, datetime):
+        self._datetime = datetime
+
+    def get_datetime(self):
+        return self._datetime
+
+    datetime = property(get_datetime, set_datetime)
+
+
+    def set_timestamp(self, timestamp):
+        self._timestamp = timestamp
+
+    def get_timestamp(self):
+        return self._timestamp
+
+    timestamp = property(get_timestamp, set_timestamp)
